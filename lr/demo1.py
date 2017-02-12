e1a9978eb7c49fed1dc49375dd55a35ce4a6702a
zuojie@mac <zuojie@ktjr.com>
2017-02-12 15:36:51 +0800

    增加lr回归的demo代码

---
+++ lr/demo1.py
@@ -0,0 +1,22 @@
+import numpy as np
+import urllib.request
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+# url with dataset
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+# download the file
+raw_data = urllib.request.urlopen(url)
+# load the CSV file as a numpy matrix
+dataset = np.loadtxt(raw_data, delimiter=",")
+# separate the data from the target attributes
+X = dataset[:,0:8]
+y = dataset[:,8]
+model = LogisticRegression()
+model.fit(X, y)
+print(model)
+# make predictions
+expected = y
+predicted = model.predict(X)
+# # summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
