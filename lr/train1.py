1d2f92e5472a8510b177e5953f24758200828c39
zuojie@mac <zuojie@ktjr.com>
2017-02-13 10:47:05 +0800

    改为模型保存,使用分开

--- lr/train1.py
+++ lr/train1.py
@@ -2,6 +2,7 @@ import numpy as np
 import urllib.request
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
 # url with dataset
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
 # download the file
@@ -14,9 +15,4 @@ y = dataset[:,8]
 model = LogisticRegression()
 model.fit(X, y)
 print(model)
-# make predictions
-expected = y
-predicted = model.predict(X)
-# # summarize the fit of the model
-print(metrics.classification_report(expected, predicted))
-print(metrics.confusion_matrix(expected, predicted))
+joblib.dump(model, 'train1.pkl')
